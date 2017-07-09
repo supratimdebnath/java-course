93c15693938ab0d71d9db14dfc0bc533aa4c3b76
Supratim Debnath <supratim_debnath@srmuniv.edu.in>
2017-07-09 15:43:12 +0530

    Add files via upload

---
+++ ideone_uvz2yk.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+import static java.lang.Math.pow;
+import java.util.Scanner;
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+
+	Scanner s=new Scanner(System.in);
+	int a=s.nextInt();
+	int b=s.nextInt();
+	double power=pow(a,b);
+	System.out.println("the value of "+a+" power "+b+" is "+power);
+
+	
+	
+	}
+}

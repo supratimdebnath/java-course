d6f8967b4f6898f68e0e2676c582778295890ca5
Supratim Debnath <supratim_debnath@srmuniv.edu.in>
2017-07-14 23:36:36 +0530

    Add files via upload

---
+++ ideone_PGMOpM.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner A=new Scanner(System.in);
+       System.out.println("enter the string to check");
+       String a=A.nextLine();
+                      
+       StringBuilder sb=new StringBuilder();
+       sb.append(a);// changes sb to a
+       String b=sb.reverse().toString();
+       
+       System.out.printf("forward integer is %s\n",a);
+       System.out.printf("reverse integer is %s\n",b);
+       
+	}
+}
